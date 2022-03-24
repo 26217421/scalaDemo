0103f3200c495be54a4818ac8448cbf74a03933b
wbw <wbw262174@gmail.com>
2022-03-25 00:01:13 +0800

    no message

--- src/main/scala/com/wbw/pbms/binarytree/Tree1.scala
+++ src/main/scala/com/wbw/pbms/binarytree/Tree1.scala
@@ -14,3 +14,23 @@ case object End extends Tree[Nothing] {
 object Node {
   def apply[T](value: T): Node[T] = Node(value, End, End)
 }
+
+object Tree{
+  //P55 (**) Construct completely balanced binary trees.
+  def cBalanced[T](nodes: Int, value: T): List[Tree[T]] = nodes match {
+    case n if n < 1 => List(End)
+    case n if n % 2 == 1 => {
+      val subtrees = cBalanced(n / 2, value)
+      subtrees.flatMap(l => subtrees.map(r => Node(value, l, r)))
+    }
+    case n if n % 2 == 0 => {2
+      val lesserSubtrees = cBalanced((n - 1) / 2, value)
+      val greaterSubtrees = cBalanced((n - 1) / 2 + 1, value)
+      lesserSubtrees.flatMap(l => greaterSubtrees.flatMap(g => List(Node(value, l, g), Node(value, g, l))))
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(Tree.cBalanced(5, "x"))
+  }
+}
